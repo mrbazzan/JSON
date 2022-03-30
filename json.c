d6151b741f65b6295f194ef8133c14ad131d8007
bazzan <bazzanapalowo@gmail.com>
2022-03-30 14:04:20 +0100

    json: typedef struct JSON
        Store the parsed values as members of a struct. Include a pointer to
        to a JSON struct as one of the parameters of core parsers.
    
        Also, store the values in heap memory so they can be freed later on.

--- json.c
+++ json.c
@@ -1,12 +1,8 @@
 
-#include <stdio.h>
-#include <math.h>
-#include <string.h>
-#include <stdlib.h>
 #include "json.h"
 
 
-str parse_number(str string){
+str parse_number(JSON *item, str string){
     double num = 0;
     int power = 0, sign = 1, inner_sign = 1, exp_power = 0;
 
@@ -35,11 +31,17 @@ str parse_number(str string){
     }
     if (*string == '\"') {string++;}
 
+    num = sign*num*pow(10, power+(inner_sign*exp_power));
+
+    item->type = NUMBER;
+    item->double_value = num;
+    item->int_value = (int)num;
+
     return string;
 }
 
 
-str parse_string(str string){
+str parse_string(JSON *item, str string){
     int len = 0;
     str ptr = string+1, str_ptr, space_ptr;
 
@@ -94,28 +96,30 @@ str parse_string(str string){
     space_ptr=0;
     if (*ptr == '\"') {ptr++;}
 
+    item->type = JSON_STRING;
+    item->str_value = str_ptr;
     return ptr;
 }
 
 
-str parse_value(str string){
+str parse_value(JSON *item, str string){
 
     if(!string){ return 0; } // error
     if(*string == '\"'){
-        return parse_string(string);
+        return parse_string(item, string);
     }
     if (*string=='-' || (*string>='0' && *string<='9')){
-        return parse_number(string);
+        return parse_number(item, string);
     }
 
-    if(*string=='{') {return parse_object(string);}
+    if(*string=='{') {return parse_object(item, string);}
 
     if (*string=='['){
-        return parse_array(string);
+        return parse_array(item, string);
     }
-    if (!strncmp(string, "true", 4)){ return string+4; }
-    if (!strncmp(string, "false", 5)){ return string+5; }
-    if (!strncmp(string, "null", 4)){ return string+4; }
+    if (!strncmp(string, "true", 4)){ item->type = TRUE; return string+4; }
+    if (!strncmp(string, "false", 5)){ item->type = FALSE; return string+5; }
+    if (!strncmp(string, "null", 4)){ item->type = JSON_NULL; return string+4; }
 
     return 0; // There is an error.
 }
@@ -128,48 +132,66 @@ str space(str item){
 }
 
 
-str parse_array(str string){
+str parse_array(JSON *item, str string){
     if(*string != '[') { return 0; } // It does not start with [
 
     // Skip whitespace
     string = space(string+1);
-    if (*string == ']'){ return string+1; } // Empty array
+
+    // Empty array
+    if (*string == ']'){ item->type = ARRAY; return string+1;}
+
+    JSON *a = calloc(sizeof(JSON), 1);
+    item->child = a; a->outer = item;
 
 ARRAY_LOOP:
-    string = space(parse_value(string));
+    string = space(parse_value(a, string));
 
     // Parse inner items
     while (*string==','){
+        JSON *b = calloc(sizeof(JSON), 1);
+        a->inner = b; b->outer = a; a=b;
+
         string = space(string+1);
         goto ARRAY_LOOP;
     }
 
     // properly parsed.
-    if(*string==']'){ return string+1; }
+    if(*string==']'){ item->type = ARRAY; return string+1;}
     return 0; // An error has occurred somewhere.
 }
 
 
-str parse_object(str string){
+str parse_object(JSON *item, str string){
 
     if(*string != '{'){ return 0; }  // not a json object
 
     string = space(string+1);
-    if(*string == '}'){ return string+1; }  // empty json object
+    if(*string == '}'){ item->type=OBJECT; return string+1; }  // empty json object
+
+    JSON *a = calloc(sizeof(JSON), 1);
+    item->child = a; a->outer = item;
 
 LOOP:
     if(*string != '\"') { return 0; } // error: not a string
-    string = space(parse_string(string));
+
+    string = space(parse_string(a, string));
+    a->key=a->str_value; a->str_value=NULL;
 
     if(*string != ':') { return 0; }  // error: ":" not present
-    string = parse_value(space(string+1));
+    string = parse_value(a, space(string+1));
+
     while(*string==','){
+
+        JSON *b = calloc(sizeof(JSON), 1);
+        a->inner = b; b->outer = a; a=b;
+
         string = space(string+1);
         goto LOOP;
     }
 
     string = space(string);
-    if (*string == '}') { return string+1; } // complete json
+    if (*string == '}') { item->type=OBJECT; return string+1; } // complete json
 
     return 0; // error!
 }
--- json.h
+++ json.h
@@ -4,10 +4,29 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define JSON_NULL 0
+#define TRUE 1
+#define FALSE 2
+#define NUMBER 3
+#define JSON_STRING 4
+#define ARRAY 5
+#define OBJECT 6
 
 typedef char * str;
 
+typedef struct JSON{
+    struct JSON *child;
+    struct JSON *inner, *outer;
+
+    unsigned int type;
+
+    int int_value;
+    double double_value;
+    str str_value;
+    str key;
+} JSON;
+
 //prototype
-str parse_value(str string);
-str parse_array(str string);
-str parse_object(str string);
+str parse_value(JSON *item, str string);
+str parse_array(JSON *item, str string);
+str parse_object(JSON *item, str string);
