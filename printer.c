8e7f61a07e6ada65d0fd72dc0d4aa5aaf210781a
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2023-01-03 12:33:22 +0100

    printer: implement "_print_string" method
    This private method contains the actual logic for printing string.

--- printer.c
+++ printer.c
@@ -35,6 +35,62 @@ str print_number(JSON *item){
 }
 
 
+str _print_string(str string){
+    int string_length = 0;
+
+    str ptr = string;
+    str output, str_ptr;
+
+    // Count the length of **string**
+    while(*ptr){
+        string_length++;
+
+        // Ensure that when a special character is encountered, 1 is added
+        // to the total length to account for its escape character. i.e
+        // The length of '\t' is 2 (\\ + \t)
+        if (*ptr < 32 || *ptr == '\\' || *ptr == '\"'){
+            string_length++;
+        }
+        ptr++;
+    }
+    ptr = string;
+
+    // we are adding 3 because of the opening, closing quote and null.
+    output = malloc(sizeof(char) * string_length+3);
+    str_ptr = output;
+
+    *str_ptr++ = '\"';
+    while(*ptr){
+        if(*ptr < 32 || *ptr == '\"' || *ptr == '\\'){
+            *str_ptr++ = '\\';
+            switch (*ptr){
+                case '\b': *str_ptr++ = 'b'; break;
+                case '\f': *str_ptr++ = 'f'; break;
+                case '\n': *str_ptr++ = 'n'; break;
+                case '\r': *str_ptr++ = 'r'; break;
+                case '\t': *str_ptr++ = 't'; break;
+                case '\"':
+                case '\\':
+                    *str_ptr++ = *ptr; break;
+
+                default:
+                    str_ptr--;  // remove the '\\'
+                    break;
+            }
+            ptr++;
+        }
+        else{
+            *str_ptr++ = *ptr++;
+        }
+    }
+
+    *str_ptr++ = '\"';
+    *str_ptr++ = 0;
+
+    return output;
+}
+
+
 extern str JSON_print(JSON *item){
     str string = 0;
     switch(item->type){
