58cc192bfcfa1217028cdf33b76951ad2fceade0
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2023-01-03 11:39:57 +0100

    printer: implement "print_number" method
    It takes a pointer to a JSON type as input, then access
    the element of the JSON struct which stores parsed number.
    The parsed number is then populated in a string buffer
    which is returned by this method.
    
    The string buffer is stored in a dynamic memory which needs to
    be free'd after printing.
    
    NB: This method tries to deal with integer, float etc. but the
    precision might be off in some cases.

--- printer.c
+++ printer.c
@@ -2,6 +2,39 @@
 #include "json.h"
 
 
+str print_number(JSON *item){
+    double d = item->double_value;
+    int length;
+
+    // In representing large number types, set aside
+    // at most 20 characters to represent number
+    //
+    str arr = malloc(64);
+
+    if (arr == NULL){
+        printf("Memory not allocated");
+        exit(1);
+    }
+
+    if (item->int_value == d){
+        length = sprintf(arr, "%d", item->int_value);
+    }else{
+        if (fabs(item->int_value - d) < 10e-6 || fabs(item->int_value -d) > 10e9){
+            length = sprintf(arr, "%e", item->double_value);
+        } else {
+            length = sprintf(arr, "%f", item->double_value);
+        }
+    }
+
+    if (length < 0){
+        printf("print number error");
+        exit(1);
+    }
+
+    return arr;
+}
+
+
 extern str JSON_print(JSON *item){
     str string = 0;
     switch(item->type){
